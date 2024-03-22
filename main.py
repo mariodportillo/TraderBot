76fadd2fbb5799cfc6302e3f1df1819c8351adca
mariodportillo <mariodportillo@gmail.com>
2024-03-21 22:54:28 -0700

    Worked on the getStockPrice(stock) function that uses yahoo_fin. I also finished up StockPortfolio class. Worked on the buyStocks function that uses the alpaca methods to make a trade.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import investpy
+from alpaca.trading.client import TradingClient
+from alpaca.trading.enums import OrderSide, TimeInForce
+from alpaca.trading.requests import MarketOrderRequest
+from datetime import date
+import config
+
+from yahoo_fin import stock_info
+
+def getStockPrice(stock):
+
+    return stock_info.get_live_price(stock)
 
 class StockPortfolio:
     def __init__(self, nameOfPortfolio):
@@ -6,27 +16,53 @@ class StockPortfolio:
         self.numOfStock = 0
         self.stockInfo = {}
 
-    def addStock(self, stock_name,
+    def addstock(self, stock_name,
                  purchase_date, num_of_shares, cost_per_share):
         self.stockInfo[stock_name] = {
             purchase_date: {num_of_shares: cost_per_share}
         }
-    def printStocks(self):
+        self.numOfStock += 1
+
+    def printstocks(self):
         print(self.stockInfo)
 
-    def deleteStock(self, deleted_stock):
+    def deletestock(self, deleted_stock):
         if deleted_stock in self.stockInfo:
-            val = input("Type YES if you want to delete stock")
+            del self.stockInfo[deleted_stock]
+        else:
+            print("This item is not in your portfolio.")
 
-            if val == "YES":
-                del self.stockInfo[deleted_stock]
+    def clear(self):
+        self.numOfStock = 0
+        self.stockInfo = {}
+
+def buyStocks(client, stock, qty, side, time_in_force, portfolio):
+    order_details = MarketOrderRequest(
+        symbol=stock,
+        qty=qty,
+        side=side,
+        time_in_force=time_in_force
+    )
+    today = date.today()
+    allPositions = client.get_all_positions()
+
+
+
+    order = client.submit_order(order_data=order_details)
+
+
+    portfolio.addstock(stock, today, qty, getStockPrice(stock))
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    test = StockPortfolio("My First Test")
-    test.addStock("DIS", "1/22/23", 30, 24.50)
-    test.printStocks()
-    test.deleteStock("DIS")
-    test.printStocks()
+    client = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
+    portfolio = StockPortfolio("Test")
+
+    buyStocks(client, "AMZN", 10, OrderSide.BUY, TimeInForce.DAY, portfolio)
+
+    portfolio.printstocks()
+    # order = client.submit_order(order_data=order_details)
+
 
 
