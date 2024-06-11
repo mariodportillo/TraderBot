ef87822616a78869e250e8d4f1942f622bb511bb
mariodportillo <mariodportillo@gmail.com>
2024-06-11 13:40:24 -0700

    Added Comments. Fixed the update stockportfolio. Updated class to properly self updadte after each trade.

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.trading.requests import MarketOrderRequest
 import config
 import requests
+import pprint
 from yahoo_fin import stock_info
 
+"""
+StockPortfolio Class which will create a portfolio for the user to manage and 
+see their current alpaca portfolio.
+Methods:
+Will add stocks to the portfolio
+addstock(self, stock_name, num_of_shares, cost_per_share)
 
+Grabs the current stockCount
+stockCount(self)
+
+printstocks(self)
+
+Will delet a stock from the portfolio
+deletestock(self, deleted_stock)
+
+clears the entire portfolio
+clear(self)
+"""
 class StockPortfolio:
     def __init__(self, nameOfPortfolio):
         self.nameOfPortfolio = nameOfPortfolio
         self.numOfStock = 0
         self.stockInfo = {}
-        dictOfStocks = getPositionInfo()
-
-        for stock in dictOfStocks.keys():
-            qty = dictOfStocks[stock]['qty']
-            price = dictOfStocks[stock]['avg_entry_price']
-            self.addstock(stock, float(qty), float(price))
+        self.updatePortfolio()
 
     def addstock(self, stock_name, num_of_shares, cost_per_share):
         if stock_name not in self.stockInfo:
@@ -35,7 +48,7 @@ class StockPortfolio:
         return self.numOfStock
 
     def printstocks(self):
-        print(self.stockInfo)
+        pprint.pprint(self.stockInfo)
 
     def deletestock(self, deleted_stock):
         if deleted_stock in self.stockInfo:
@@ -47,11 +60,30 @@ class StockPortfolio:
         self.numOfStock = 0
         self.stockInfo = {}
 
+    def updatePortfolio(self):
+        self.clear()
 
+        dictOfStocks = getPositionInfo()
+
+        for stock in dictOfStocks.keys():
+            qty = dictOfStocks[stock]['qty']
+            price = dictOfStocks[stock]['avg_entry_price']
+            self.addstock(stock, float(qty), float(price))
+
+
+"""
+Grabs the current stock price of whatever stock is inputted. 
+"""
 def getStockPrice(stock):
+    """
+    >>> getStockPrice("AAPL")
+    205.21560668945312
+    """
     return stock_info.get_live_price(stock)
 
-
+"""
+Grabs the current position info based on the alpacca client
+"""
 def getPositionInfo():
     url = "https://paper-api.alpaca.markets/v2/positions"
 
@@ -82,7 +114,9 @@ def getPositionInfo():
         print("Failed to fetch position information:", response.text)
         return None
 
-
+"""
+Buys a stock and makes an order
+"""
 def buyStocks(tradingClient, stock, qty, side, time_in_force, NamePortfolio):
     order_details = MarketOrderRequest(
         symbol=stock,
@@ -94,6 +128,9 @@ def buyStocks(tradingClient, stock, qty, side, time_in_force, NamePortfolio):
     order = tradingClient.submit_order(order_data=order_details)
     NamePortfolio.addstock(stock, qty, getStockPrice(stock))
 
+"""
+Sells a stock and makes an order
+"""
 def sellStocks(tradingClient, stock, qty, side, time_in_force, NamePortfolio):
     order_details = MarketOrderRequest(
         symbol=stock,
@@ -103,14 +140,13 @@ def sellStocks(tradingClient, stock, qty, side, time_in_force, NamePortfolio):
     )
 
     order = tradingClient.submit_order(order_data=order_details)
-    NamePortfolio.addstock(stock, qty, getStockPrice(stock))
+    NamePortfolio.updatePortfolio()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     client = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
     portfolio = StockPortfolio("Test")
-    portfolio.printstocks()
-    buyStocks(client, "AMZN", 1, OrderSide.BUY, TimeInForce.DAY, portfolio)
 
-    # portfolio.printstocks()
+    # buyStocks(client, "AMZN", 1, OrderSide.BUY, TimeInForce.DAY, portfolio)
     # order = client.submit_order(order_data=order_details)
